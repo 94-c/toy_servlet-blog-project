cdeb1a3857a5b72bb46b8c7b17b42aa6c1b310d7
94-c <hyeongwoo26@gmail.com>
2022-08-15 23:37:32 +0900

    게시물 테스트 코드 작성(DAO)

---
+++ src/test/java/com/blog/dao/PostDAOTest.java
@@ -0,0 +1,30 @@
+package com.blog.dao;
+
+import com.blog.dto.PostDTO;
+import com.blog.dto.UserDTO;
+import com.blog.entity.Post;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class PostDAOTest {
+
+    private final PostDAO postDAO = new PostDAO();
+
+    @Test
+    public void create() {
+
+    }
+
+    @Test
+    public void update() {
+    }
+
+    @Test
+    public void findAllCreateQuery() {
+    }
+
+    @Test
+    public void deleteUpdate() {
+    }
+}
