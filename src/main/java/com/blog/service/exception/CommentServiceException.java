075af09c05c31e5c3233f31ed6129251f0986fd7
94-c <hyeongwoo26@gmail.com>
2022-09-03 22:01:20 +0900

    exception 처리

--- src/main/java/com/blog/service/exception/CommentServiceException.java
+++ src/main/java/com/blog/service/exception/CommentServiceException.java
@@ -1,13 +1,16 @@
 package com.blog.service.exception;
 
+import com.sun.org.slf4j.internal.LoggerFactory;
+
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class CommentServiceException extends ServiceException {
-    private final Logger logger;
 
-    /*public CommentServiceException(String message) {
-        this.logger = new jdk.jfr.internal.Logger()
-        logger.log("error", message);
+    private final Logger log = Logger.getLogger("log4j.properties");
+    public CommentServiceException(String message, Level level) {
         super(message);
-    }*/
+        log.log(level, message);
+    }
+
 }
